d21d8163ab4908a09f8bab9bc8314d092868dad4
Gerson Calvo <gedacaba@gmail.com>
2023-03-21 15:40:58 -0500

    curso fundamentos de c#

---
+++ Entidades/Curso.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreEscuela.Entidades
+{
+    public class Curso
+    {
+
+         public string UniqueId { get; private set; }
+        public string Nombre { get; set; }
+        public TiposJornada Jornada { get; set; }
+        
+        public Curso()=> UniqueId = Guid.NewGuid().ToString();
+        
+    }
+}
---
+++ Entidades/Escuela.cs
@@ -0,0 +1,43 @@
+namespace CoreEscuela.Entidades
+{
+    class Escuela
+    {
+
+        string nombre;
+
+        //encapsular para poder ser accedido
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value.ToUpper(); }
+        }
+
+        public int AñoDeCreación { get; set; }
+
+        public string? Pais { get; set; }
+        public string? Ciudad { get; set; }
+
+        public TiposEscuela TiposEscuela {get; set;}
+
+        //proviene de la clase curso
+        public Curso[] Cursos { get; set; }
+
+        //contructor de la clase
+        public Escuela(string nombre, int año) => (Nombre, AñoDeCreación) = (nombre, año);
+
+         public override string ToString()
+        {
+            return $"Nombre: \"{Nombre}\", Tipo: {TiposEscuela} {System.Environment.NewLine} Pais: {Pais}, Ciudad:{Ciudad}";
+        }
+
+        public Escuela(string nombre, int año, 
+                       TiposEscuela tipo, 
+                       string pais = "", string ciudad = "")
+        {
+            (Nombre, AñoDeCreación) = (nombre, año);
+            Pais = pais;
+            Ciudad = ciudad;
+        }
+
+    }
+}
