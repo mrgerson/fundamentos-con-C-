43921621eb4911e9e682d20bf9bdf05437f8829b
Gerson Calvo <gedacaba@gmail.com>
2023-03-24 16:10:08 -0500

    cusro completo de la escuela

--- Entidades/Curso.cs
+++ Entidades/Curso.cs
@@ -1,17 +1,15 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CoreEscuela.Entidades
 {
     public class Curso
     {
-
-         public string UniqueId { get; private set; }
+        public string UniqueId { get; private set; }
         public string Nombre { get; set; }
         public TiposJornada Jornada { get; set; }
-        
+        public List<Asignatura> Asignaturas{ get; set; }
+        public List<Alumno> Alumnos{ get; set; }
         public Curso()=> UniqueId = Guid.NewGuid().ToString();
         
     }
--- Entidades/Escuela.cs
+++ Entidades/Escuela.cs
@@ -1,46 +1,38 @@
+using System;
 using System.Collections.Generic;
 
 namespace CoreEscuela.Entidades
 {
-    class Escuela
+    public class Escuela
     {
-
+        public string UniqueId { get; private set; } = Guid.NewGuid().ToString();
         string nombre;
-
-        //encapsular para poder ser accedido
         public string Nombre
         {
-            get { return nombre; }
+            get { return "Copia:" + nombre; }
             set { nombre = value.ToUpper(); }
         }
-
         public int AñoDeCreación { get; set; }
 
-        public string? Pais { get; set; }
-        public string? Ciudad { get; set; }
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+        public TiposEscuela TipoEscuela { get; set; }
+        public List<Curso> Cursos { get; set; }
 
-        public TiposEscuela TiposEscuela {get; set;}
-
-        //proviene de la clase curso
-        /* public Curso[] Cursos { get; set; } */
-        public List<Curso> Cursos {get; set;}
-
-        //contructor de la clase
         public Escuela(string nombre, int año) => (Nombre, AñoDeCreación) = (nombre, año);
-
-         public override string ToString()
-        {
-            return $"Nombre: \"{Nombre}\", Tipo: {TiposEscuela} {System.Environment.NewLine} Pais: {Pais}, Ciudad:{Ciudad}";
-        }
 
         public Escuela(string nombre, int año, 
                        TiposEscuela tipo, 
-                       string pais = "", string ciudad = "")
+                       string pais = "", string ciudad = "") : base()
         {
             (Nombre, AñoDeCreación) = (nombre, año);
             Pais = pais;
             Ciudad = ciudad;
         }
 
+        public override string ToString()
+        {
+            return $"Nombre: \"{Nombre}\", Tipo: {TipoEscuela} {System.Environment.NewLine} Pais: {Pais}, Ciudad:{Ciudad}";
+        }
     }
 }
